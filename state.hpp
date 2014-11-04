ef778508cc2cf2478dec06af7d8d102e88068984
Andrew Morris <voltrevo@gmail.com>
2014-11-04 16:57:45 +1100

    Storing a uint16_t per cell is >2x faster

--- state.hpp
+++ state.hpp
@@ -13,50 +13,26 @@ private:
     using u8 = std::uint8_t;
     using u16 = std::uint16_t;
 
-    std::array<u8, 92> mBytes;
+    std::array<u16, 81> mCells;
 
 public:
     State()
     {
         // Set all flags to true
-        for (auto& b : mBytes)
+        for (auto& c : mCells)
         {
-            b = 255;
+            c = 511;
         }
     }
 
     u16 GetCell(u16 cellNo) const
     {
-        u16 skipBits = cellNo * u16(9);
-        u8 skipBytes = skipBits / u16(8);
-
-        u16 state = (mBytes[skipBytes] << u16(8)) + mBytes[skipBytes + u8(1)];
-
-        u8 shiftBits = u16(7) - (skipBits - skipBytes * u16(8));
-
-        state = (state & (u16(511) << shiftBits)) >> shiftBits;
-
-        return state;
+        return mCells[cellNo];
     }
 
     void SetCell(u16 cellNo, u16 state)
     {
-        u16 skipBits = cellNo * u16(9);
-        u8 skipBytes = skipBits / u16(8);
-
-        u8& byte1 = mBytes[skipBytes];
-        u8& byte2 = mBytes[skipBytes + u8(1)];
-
-        u8 shiftBits = u16(7) - (skipBits - skipBytes * u16(8));
-        state = state << shiftBits;
-
-        u16 mask = ~(u16(511) << shiftBits);
-
-        byte1 = byte1 & (mask >> u16(8));
-        byte1 = byte1 | (state >> u16(8));
-
-        byte2 = byte2 & ((mask << u16(8)) >> u16(8));
-        byte2 = byte2 | ((state << u16(8)) >> u16(8));
+        mCells[cellNo] = state;
     }
 
     u8 PickNonFinalisedCell()
