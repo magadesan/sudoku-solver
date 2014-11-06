6260be4172426e4154b4d8e9fd96ddb03a0fe428
Andrew Morris <voltrevo@gmail.com>
2014-11-06 20:09:05 +1100

    CallbackQueue now 128 bits instead of 32 bits in preparation for cell-based processing instead of group-based processing.

--- callbackQueue.hpp
+++ callbackQueue.hpp
@@ -8,35 +8,54 @@ class CallbackQueue
 {
 private:
     using u8 = std::uint8_t;
-    using u32 = std::uint32_t;
-    u32 mFlags = 0u;
+    using u64 = std::uint64_t;
+    std::array<u64, 2> mFlags = {{0u, 0u}};
 
 public:
-    bool Empty() const { return mFlags == 0u; }
+    bool Empty() const { return mFlags[0] == 0u && mFlags[1] == 0u; }
     
-    u8 Size() const { return __builtin_popcount(mFlags); }
+    u8 Size() const { return __builtin_popcountll(mFlags[0]) + __builtin_popcountll(mFlags[1]); }
     
     void Push(u8 x)
     {
-        assert(x < 27u);
-        mFlags |= 1u << x;
+        assert(x < 81u);
+        bool first = x < 64u;
+        u64 bit = 1u << (x % 64u);
+        mFlags[0] |= first * bit;
+        mFlags[1] |= !first * bit;
     }
 
-    void PushFlags(u32 flags) { mFlags |= flags; }
+    void PushFlags(std::array<u64, 2> flags) { mFlags[0] |= flags[0]; mFlags[1] |= flags[1]; }
+    void PushFlags(u64 flags) { mFlags[0] |= flags; } // TODO: remove
     
     u8 Pop()
     {
-        if (Empty())
-            return u8(255);
+        if (mFlags[0] != 0u)
+        {
+            u8 ret = __builtin_ctzll(mFlags[0]);
+            mFlags[0] &= ~(1u << ret);
+            return u8(ret);
+        }
+        else if (mFlags[1] != 0u)
+        {
+            u8 ret = __builtin_ctzll(mFlags[1]);
+            mFlags[1] &= ~(1u << ret);
+            return u8(64u + ret);
+        }
 
-        u8 ret = __builtin_ctz(mFlags);
-        PopElement(ret);
-        
-        return ret;
+        return u8(255);
     }
 
     void PopElement(u8 element)
     {
-        mFlags &= ~(1u << element);
+        if (element < 64u)
+        {
+            mFlags[0] &= ~(1u << element);
+        }
+        else
+        {
+            element -= 64u;
+            mFlags[1] &= ~(1u << element);
+        }
     }
 };
